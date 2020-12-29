1ed69df05564f9ff63f607ac191020bb52bbe777
Sławomir Brzozowski <brzozasr@gmail.com>
2020-12-29 12:38:59 +0100

    Partially implemented filtering processes.

--- src/Codecool.ProcessWatch/GUI/Gui.cs
+++ src/Codecool.ProcessWatch/GUI/Gui.cs
@@ -13,6 +13,7 @@ namespace Codecool.ProcessWatch.GUI
         private List<MemoryItemProcess> _processesList = new List<MemoryItemProcess>();
         private int _numberOfPages;
         private int _pageNo;
+        private string _filterType = "AllProcesses";
         private readonly Label _pageNoLbl;
         private readonly Button _previousBtn;
         private readonly Button _nextBtn;
@@ -25,6 +26,14 @@ namespace Codecool.ProcessWatch.GUI
         private SpinButton _startAtDateYearSb;
         private SpinButton _startAtDaySb;
         private SpinButton _startAtMonthSb;
+        private SpinButton _startBeforeDateDaySb;
+        private SpinButton _startBeforeDateMonthSb;
+        private SpinButton _startBeforeDateYearSb;
+        private SpinButton _startAfterDateDaySb;
+        private SpinButton _startAfterDateMonthSb;
+        private SpinButton _startAfterDateYearSb;
+        private SpinButton _memoryUsageSb;
+        private Label _memoryUsageLbl;
 
         public Gui() : base("Process Watch - GUI")
         {
@@ -73,7 +82,7 @@ namespace Codecool.ProcessWatch.GUI
             view.AppendColumn("Start Time", centerAlignment, "text", 8);
             view.AppendColumn("Base Priority", rightAlignment, "text", 9);
             // view.Show();
-            
+
             scrolled.Add(view);
 
             VBox mainVBox = new VBox(false, 0);
@@ -86,7 +95,7 @@ namespace Codecool.ProcessWatch.GUI
             _filterCb = new ComboBox(_cbStore);
             _filterCb.Active = 0;
             Label emptyLbl = new Label();
-            
+
             Fixed topLeftFixed = new Fixed();
 
             topLeftFixed.Put(filterLbl, 10, 10);
@@ -140,7 +149,7 @@ namespace Codecool.ProcessWatch.GUI
 
             Add(mainVBox);
             ShowAll();
-            
+
             // loop for updating GUI
             // while (Gtk.Application.EventsPending())
             // {
@@ -153,6 +162,7 @@ namespace Codecool.ProcessWatch.GUI
             _searchEntry = new Entry();
             _searchEntry.Visible = false;
             _topRightHBox.Add(_searchEntry);
+            _searchEntry.Changed += OnChangeSearchEntry;
 
             _startAtDateDaySb = new SpinButton(1, 31, 1);
             _startAtDateDaySb.Value = DateTime.Now.Day;
@@ -166,16 +176,78 @@ namespace Codecool.ProcessWatch.GUI
             _topRightHBox.Add(_startAtDateDaySb);
             _topRightHBox.Add(_startAtDateMonthSb);
             _topRightHBox.Add(_startAtDateYearSb);
-            
+            _startAtDateDaySb.Changed += OnChangeAtDateDay;
+            _startAtDateMonthSb.Changed += OnChangeAtDateMonth;
+            _startAtDateYearSb.Changed += OnChangeAtDateYear;
+
             _startAtDaySb = new SpinButton(1, 31, 1);
             _startAtDaySb.Value = DateTime.Now.Day;
             _startAtDaySb.Visible = false;
             _topRightHBox.Add(_startAtDaySb);
-            
+
             _startAtMonthSb = new SpinButton(1, 12, 1);
             _startAtMonthSb.Value = DateTime.Now.Month;
             _startAtMonthSb.Visible = false;
             _topRightHBox.Add(_startAtMonthSb);
+
+            _startBeforeDateDaySb = new SpinButton(1, 31, 1);
+            _startBeforeDateDaySb.Value = DateTime.Now.Day;
+            _startBeforeDateDaySb.Visible = false;
+            _startBeforeDateMonthSb = new SpinButton(1, 12, 1);
+            _startBeforeDateMonthSb.Value = DateTime.Now.Month;
+            _startBeforeDateMonthSb.Visible = false;
+            _startBeforeDateYearSb = new SpinButton(1990, 2200, 1);
+            _startBeforeDateYearSb.Value = DateTime.Now.Year;
+            _startBeforeDateYearSb.Visible = false;
+            _topRightHBox.Add(_startBeforeDateDaySb);
+            _topRightHBox.Add(_startBeforeDateMonthSb);
+            _topRightHBox.Add(_startBeforeDateYearSb);
+
+            _startAfterDateDaySb = new SpinButton(1, 31, 1);
+            _startAfterDateDaySb.Value = DateTime.Now.Day;
+            _startAfterDateDaySb.Visible = false;
+            _startAfterDateMonthSb = new SpinButton(1, 12, 1);
+            _startAfterDateMonthSb.Value = DateTime.Now.Month;
+            _startAfterDateMonthSb.Visible = false;
+            _startAfterDateYearSb = new SpinButton(1990, 2200, 1);
+            _startAfterDateYearSb.Value = DateTime.Now.Year;
+            _startAfterDateYearSb.Visible = false;
+            _topRightHBox.Add(_startAfterDateDaySb);
+            _topRightHBox.Add(_startAfterDateMonthSb);
+            _topRightHBox.Add(_startAfterDateYearSb);
+
+            Adjustment memory = new Adjustment(1.0, 0.0001, 99999.9999, 1, 1, 1);
+            _memoryUsageSb = new SpinButton(memory, 0.0, 4);
+            _memoryUsageSb.Numeric = true;
+            _memoryUsageSb.Visible = false;
+            _memoryUsageLbl = new Label("  1 MB");
+            _topRightHBox.Add(_memoryUsageSb);
+            _topRightHBox.Add(_memoryUsageLbl);
+            _memoryUsageSb.Changed += OnChangeMemoryUsage;
+        }
+
+        private void HideAllWidgets()
+        {
+            _searchEntry.Visible = false;
+
+            _startAtDateDaySb.Visible = false;
+            _startAtDateMonthSb.Visible = false;
+            _startAtDateYearSb.Visible = false;
+
+            _startAtDaySb.Visible = false;
+
+            _startAtMonthSb.Visible = false;
+
+            _startBeforeDateDaySb.Visible = false;
+            _startBeforeDateMonthSb.Visible = false;
+            _startBeforeDateYearSb.Visible = false;
+
+            _startAfterDateDaySb.Visible = false;
+            _startAfterDateMonthSb.Visible = false;
+            _startAfterDateYearSb.Visible = false;
+
+            _memoryUsageSb.Visible = false;
+            _memoryUsageLbl.Visible = false;
         }
 
         private void OnChanged(object sender, EventArgs e)
@@ -190,127 +262,189 @@ namespace Codecool.ProcessWatch.GUI
             switch (_cbStore[cb.Active])
             {
                 case "- processes show all":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    _filterType = "AllProcesses";
+                    HideAllWidgets();
+                    _pageNo = 1;
+                    InitAllProcesses();
                     break;
                 case "- processes name":
+                    _filterType = "ProcessesByName";
+                    HideAllWidgets();
                     _searchEntry.Visible = true;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    _pageNo = 1;
+                    InitProcessesByName();
                     break;
                 case "- processes started at date":
-                    _searchEntry.Visible = false;
-                    
+                    _filterType = "ProcessesStartAtDate";
+                    HideAllWidgets();
                     _startAtDateDaySb.Visible = true;
                     _startAtDateMonthSb.Visible = true;
                     _startAtDateYearSb.Visible = true;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    _pageNo = 1;
+                    InitProcessesAtDate();
                     break;
                 case "- processes started at day":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
+                    HideAllWidgets();
                     _startAtDaySb.Visible = true;
-                    
-                    _startAtMonthSb.Visible = false;
                     break;
                 case "- processes started at month":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
+                    HideAllWidgets();
                     _startAtMonthSb.Visible = true;
                     break;
                 case "- processes started before date":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    HideAllWidgets();
+                    _startBeforeDateDaySb.Visible = true;
+                    _startBeforeDateMonthSb.Visible = true;
+                    _startBeforeDateYearSb.Visible = true;
                     break;
                 case "- processes started after date":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    HideAllWidgets();
+                    _startAfterDateDaySb.Visible = true;
+                    _startAfterDateMonthSb.Visible = true;
+                    _startAfterDateYearSb.Visible = true;
                     break;
                 case "- processes physical memory usage greater than...":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    HideAllWidgets();
+                    _memoryUsageSb.Visible = true;
+                    _memoryUsageLbl.Visible = true;
                     break;
                 case "- processes user CPU time greater than...":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    HideAllWidgets();
                     break;
                 case "- processes total CPU time greater than...":
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    HideAllWidgets();
                     break;
                 default:
-                    _searchEntry.Visible = false;
-                    
-                    _startAtDateDaySb.Visible = false;
-                    _startAtDateMonthSb.Visible = false;
-                    _startAtDateYearSb.Visible = false;
-                    
-                    _startAtDaySb.Visible = false;
-                    
-                    _startAtMonthSb.Visible = false;
+                    HideAllWidgets();
                     break;
             }
+        }
+
+        private void OnChangeCommonMethod()
+        {
+            RemoveAllDataView();
+            _pageNo = 1;
+            RefreshView();
+            if (_numberOfPages == 0)
+            {
+                _pageNoLbl.Text = $"0 of {_numberOfPages}";
+            }
+            else
+            {
+                _pageNoLbl.Text = $"{_pageNo} of {_numberOfPages}";
+            }
+
+            SetNaviBtnSensitive();
+        }
+
+        private void OnChangeSearchEntry(object sender, EventArgs e)
+        {
+            OnChangeCommonMethod();
+        }
+
+        private void OnChangeMemoryUsage(object sender, EventArgs e)
+        {
+            string txt = "";
+            double memory = _memoryUsageSb.Value;
+
+            if (memory > 1024 * 1024)
+            {
+                memory = memory / 1024 / 1024;
+                txt = $"  {Math.Round(memory, 2)} TB";
+            }
+            else if (memory > 1024)
+            {
+                memory = memory / 1024;
+                txt = $"  {Math.Round(memory, 2)} GB";
+            }
+            else if (memory >= 1 && memory <= 1024)
+            {
+                txt = $"  {Math.Round(memory, 2)} MB";
+            }
+            else if (memory < 1)
+            {
+                memory = memory * 1024;
+                txt = $"  {Math.Round(memory, 2)} kB";
+            }
+
+            _memoryUsageLbl.Text = txt;
+        }
+
+        private void OnChangeAtDateYear(object sender, EventArgs e)
+        {
+            OnChangeCommonMethod();
+        }
+
+        private void OnChangeAtDateMonth(object sender, EventArgs e)
+        {
+            OnChangeCommonMethod();
+        }
+
+        private void OnChangeAtDateDay(object sender, EventArgs e)
+        {
+            OnChangeCommonMethod();
+        }
+
+        private void InitAllProcesses()
+        {
+            RemoveAllDataView();
+            var processesData = ProcessWatchApplication.AllProcesses(PageSize, _pageNo);
+            _processesList = processesData.ProcessesList;
+            _numberOfPages = processesData.NumberOfPages;
+            if (_numberOfPages == 0)
+            {
+                _pageNoLbl.Text = $"0 of {_numberOfPages}";
+            }
+            else
+            {
+                _pageNoLbl.Text = $"{_pageNo} of {_numberOfPages}";
+            }
+
+            _pageNoLbl.Text = $"{_pageNo} of {_numberOfPages}";
+            RefreshView();
+            SetNaviBtnSensitive();
+        }
+
+        private void InitProcessesByName()
+        {
+            RemoveAllDataView();
+            var processesData = ProcessWatchApplication.SelectProcessesByName(PageSize, _pageNo, _searchEntry.Text);
+            _processesList = processesData.ProcessesList;
+            _numberOfPages = processesData.NumberOfPages;
+            if (_numberOfPages == 0)
+            {
+                _pageNoLbl.Text = $"0 of {_numberOfPages}";
+            }
+            else
+            {
+                _pageNoLbl.Text = $"{_pageNo} of {_numberOfPages}";
+            }
+
+            _pageNoLbl.Text = $"{_pageNo} of {_numberOfPages}";
+            RefreshView();
+            SetNaviBtnSensitive();
+        }
+
+        private void InitProcessesAtDate()
+        {
+            RemoveAllDataView();
+            var processesStartAtDate = ProcessWatchApplication.SelectProcessesStartAtDate(PageSize, _pageNo,
+                (int) _startAtDateDaySb.Value, (int) _startAtDateMonthSb.Value,
+                (int) _startAtDateYearSb.Value);
+            _processesList = processesStartAtDate.ProcessesList;
+            _numberOfPages = processesStartAtDate.NumberOfPages;
+            if (_numberOfPages == 0)
+            {
+                _pageNoLbl.Text = $"0 of {_numberOfPages}";
+            }
+            else
+            {
+                _pageNoLbl.Text = $"{_pageNo} of {_numberOfPages}";
+            }
+
+            _pageNoLbl.Text = $"{_pageNo} of {_numberOfPages}";
+            RefreshView();
+            SetNaviBtnSensitive();
         }
 
         private void OnClickPreviousBtn(object sender, EventArgs e)
@@ -341,9 +475,11 @@ namespace Codecool.ProcessWatch.GUI
             else if (_pageNo == 1)
             {
                 _previousBtn.Sensitive = false;
+                _nextBtn.Sensitive = true;
             }
             else if (_pageNo == _numberOfPages)
             {
+                _previousBtn.Sensitive = true;
                 _nextBtn.Sensitive = false;
             }
             else
@@ -403,7 +539,8 @@ namespace Codecool.ProcessWatch.GUI
 
         private void RefreshView()
         {
-            _processesList = ProcessWatchApplication.AllProcesses(PageSize, _pageNo).ProcessesList;
+            // _processesList = ProcessWatchApplication.AllProcesses(PageSize, _pageNo).ProcessesList;
+            GetProcessesList();
 
             foreach (var p in _processesList)
             {
@@ -418,6 +555,36 @@ namespace Codecool.ProcessWatch.GUI
                     Converters.IntNullConverter(p.ThreadsNumber),
                     Converters.DateTimeNullConverter(p.StartTime),
                     Converters.IntNullConverter(p.BasePriority)));
+            }
+        }
+
+        private void GetProcessesList()
+        {
+            switch (_filterType)
+            {
+                case "AllProcesses":
+                    var processesAll = ProcessWatchApplication.AllProcesses(PageSize, _pageNo);
+                    _processesList = processesAll.ProcessesList;
+                    _numberOfPages = processesAll.NumberOfPages;
+                    break;
+                case "ProcessesByName":
+                    var processesName =
+                        ProcessWatchApplication.SelectProcessesByName(PageSize, _pageNo, _searchEntry.Text);
+                    _processesList = processesName.ProcessesList;
+                    _numberOfPages = processesName.NumberOfPages;
+                    break;
+                case "ProcessesStartAtDate":
+                    var processesStartAtDate = ProcessWatchApplication.SelectProcessesStartAtDate(PageSize, _pageNo,
+                        (int) _startAtDateDaySb.Value, (int) _startAtDateMonthSb.Value,
+                        (int) _startAtDateYearSb.Value);
+                    _processesList = processesStartAtDate.ProcessesList;
+                    _numberOfPages = processesStartAtDate.NumberOfPages;
+                    break;
+                default:
+                    var processesDefault = ProcessWatchApplication.AllProcesses(PageSize, _pageNo);
+                    _processesList = processesDefault.ProcessesList;
+                    _numberOfPages = processesDefault.NumberOfPages;
+                    break;
             }
         }
 
