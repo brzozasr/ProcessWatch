e58e4f182ec8ca92b16e5cf241e3f660517f0543
Sławomir Brzozowski <brzozasr@gmail.com>
2021-01-01 20:55:55 +0100

    GUI help information was fulfilled and some small improvement also was implemented.

--- src/Codecool.ProcessWatch/GUI/AboutApp.cs
+++ src/Codecool.ProcessWatch/GUI/AboutApp.cs
@@ -151,9 +151,9 @@ Michał Orłowski");
         private static string License()
         {
             return @"
-                                            Apache License
-                                        Version 2.0, January 2004
-                                      http://www.apache.org/licenses/
+                                                            Apache License
+                                                        Version 2.0, January 2004
+                                                      http://www.apache.org/licenses/
 
    TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
 
--- src/Codecool.ProcessWatch/GUI/HelpApp.cs
+++ src/Codecool.ProcessWatch/GUI/HelpApp.cs
@@ -13,19 +13,88 @@ namespace Codecool.ProcessWatch.GUI
             this.Resizable = false;
 
             TextView helpTextView = new TextView();
+            ScrolledWindow scrolledWindow = new ScrolledWindow();
             helpTextView.Editable = false;
             helpTextView.Buffer.Text = HelpText();
+            scrolledWindow.Add(helpTextView);
             
-            Add(helpTextView);
+            Add(scrolledWindow);
             ShowAll();
         }
 
         private string HelpText()
         {
             return @"
-            Help for application ""Process Watch""
+                                                                      HELP
+                                                                ""Process Watch""
 
-";
+    Buttons' top menu:
+        - Run / Stop auto refresh view: start or stop automatic process view refresh.
+            The view is refreshed every half second.
+
+        - Refresh view: manual refreshing of the process view.
+
+        - Help: information on how to operate the application.
+
+        - About application: information about the application version, authors, 
+            website and license.
+
+
+    Available filters:
+        - processes show all: all available processes on the computer.
+
+        - processes name: filtering / searching processes by name.
+
+        - processes started at date: filtering / searching for processes that 
+            started on a given date.
+
+        - processes started at day: filtering / searching for processes that 
+            have started on a given day takes into account all processes 
+            from each month.
+
+        - processes started at month: filtering / searching for processes that 
+            have started in a given month includes all processes from each year.
+
+        - processes started before date: filtering / searching for processes that 
+            started before the given date. The current hour and minutes are used 
+            for filtering.
+
+        - processes started after date: filtering / searching for processes that 
+            started after the given date. The current hour and minutes are used 
+            for filtering.
+
+        - processes physical memory usage greater than: filtering / searching for 
+            processes that physical memory usage is greater than given value.
+
+        - processes user CPU time greater than: filtering / searching for processes 
+            that user CPU time is greater than given time.
+
+        - processes total CPU time greater than: filtering / searching for processes 
+            that total CPU time is greater than given time.
+
+
+    Processes view:
+        Double-clicking on a selected process in the process view adds it to 
+            the list of processes to be killed.
+
+
+    Tools for killing processes:
+        - The label with ""Number processes to kill"": shows the number of selected 
+            processes to kill.
+
+        - The button ""Show processes to kill"": shows the list of selected processes 
+            to be stopped (process number and process name).
+
+        - The button ""Clear processes to kill"": removes all selected processes 
+            to be killed from the list.
+
+        - The button ""Kill selected"": kills all processes from the list.
+
+
+    Pagination buttons:
+        The previous and next buttons switch to other pages on the list of processes.
+
+    ";
         }
     }
 }
